5064bd29e539ef3f4a0b9ee2d5f83813193f4989
Lai Weng Han <jlwhoo7@gmail.com>
2019-04-03 22:32:32 +0800

    move helper functions into seperate folder

---
+++ src/helper/helper.go
@@ -0,0 +1,39 @@
+package helper
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+// helper
+// doReq HTTP client
+func doReq(req *http.Request) ([]byte, error) {
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	if 200 != resp.StatusCode {
+		return nil, fmt.Errorf("%s", body)
+	}
+	return body, nil
+}
+
+// MakeReq HTTP request helper
+func MakeReq(url string) ([]byte, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := doReq(req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, err
+}
--- src/v3/v3.go
+++ src/v3/v3.go
@@ -3,10 +3,9 @@ package coingecko
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"strings"
 
+	helper "github.com/superoo7/go-gecko/src/helper"
 	types "github.com/superoo7/go-gecko/src/v3/types"
 )
 
@@ -15,7 +14,7 @@ var baseURL = "https://api.coingecko.com/api/v3"
 // Ping /ping endpoint
 func Ping() (*types.Ping, error) {
 	url := fmt.Sprintf("%s/ping", baseURL)
-	resp, err := makeReq(url)
+	resp, err := helper.MakeReq(url)
 	if err != nil {
 		return nil, err
 	}
@@ -33,7 +32,7 @@ func SimpleSinglePrice(cID string, vC string) (*types.SimpleSinglePrice, error) 
 	vC = strings.ToLower(vC)
 	params := []string{fmt.Sprintf("ids=%s", cID), fmt.Sprintf("vs_currencies=%s", vC)}
 	url := fmt.Sprintf("%s/simple/price?%s", baseURL, strings.Join(params, "&"))
-	resp, err := makeReq(url)
+	resp, err := helper.MakeReq(url)
 	if err != nil {
 		return nil, err
 	}
@@ -45,36 +44,4 @@ func SimpleSinglePrice(cID string, vC string) (*types.SimpleSinglePrice, error) 
 	c := t[cID]
 	data := &types.SimpleSinglePrice{ID: cID, Currency: vC, MarketPrice: c[vC]}
 	return data, nil
-}
-
-// helper
-// doReq HTTP client
-func doReq(req *http.Request) ([]byte, error) {
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	if 200 != resp.StatusCode {
-		return nil, fmt.Errorf("%s", body)
-	}
-	return body, nil
-}
-
-// makeReq HTTP request helper
-func makeReq(url string) ([]byte, error) {
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, err
-	}
-	resp, err := doReq(req)
-	if err != nil {
-		return nil, err
-	}
-	return resp, err
 }
