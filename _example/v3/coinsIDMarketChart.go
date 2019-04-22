cf150834681c0931d4959fcbf84ded31239918c5
Vologzhanin Vladimir <vvv@oreolo.com>
2019-04-22 03:38:26 +0300

    add market chart

---
+++ _example/v3/coinsIDMarketChart.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	gecko "github.com/superoo7/go-gecko/v3"
+)
+
+func main() {
+	m, err := gecko.CoinsIDMarketChart("bitcoin", "usd", "1")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Prices\n")
+	for _, v := range *m.Prices {
+		fmt.Printf("%s:%.04f\n", time.Unix(int64(v[0])/1000, int64(v[0])%1000).UTC().Format(time.RFC3339), v[1])
+	}
+
+	fmt.Printf("MarketCaps\n")
+	for _, v := range *m.MarketCaps {
+		fmt.Printf("%s:%.04f\n", time.Unix(int64(v[0])/1000, int64(v[0])%1000).UTC().Format(time.RFC3339), v[1])
+	}
+
+	fmt.Printf("TotalVolumes\n")
+	for _, v := range *m.TotalVolumes {
+		fmt.Printf("%s:%.04f\n", time.Unix(int64(v[0])/1000, int64(v[0])%1000).UTC().Format(time.RFC3339), v[1])
+	}
+}
