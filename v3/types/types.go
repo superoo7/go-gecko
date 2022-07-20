1179234035497020198c899579845a85b2b6ce70
Dhruv Chauhan <dhruv.chauhan98@gmail.com>
2022-07-21 00:14:18 +0530

    add SentimentVotes to CoinsID struct

--- v3/types/types.go
+++ v3/types/types.go
@@ -24,28 +24,30 @@ type CoinsMarket []CoinsMarketItem
 // CoinsID https://api.coingecko.com/api/v3/coins/bitcoin
 type CoinsID struct {
 	coinBaseStruct
-	BlockTimeInMin      int32               `json:"block_time_in_minutes"`
-	Categories          []string            `json:"categories"`
-	Localization        LocalizationItem    `json:"localization"`
-	Description         DescriptionItem     `json:"description"`
-	Links               *LinksItem          `json:"links"`
-	Image               ImageItem           `json:"image"`
-	CountryOrigin       string              `json:"country_origin"`
-	GenesisDate         string              `json:"genesis_date"`
-	MarketCapRank       uint16              `json:"market_cap_rank"`
-	CoinGeckoRank       uint16              `json:"coingecko_rank"`
-	CoinGeckoScore      float32             `json:"coingecko_score"`
-	DeveloperScore      float32             `json:"developer_score"`
-	CommunityScore      float32             `json:"community_score"`
-	LiquidityScore      float32             `json:"liquidity_score"`
-	PublicInterestScore float32             `json:"public_interest_score"`
-	MarketData          *MarketDataItem     `json:"market_data"`
-	CommunityData       *CommunityDataItem  `json:"community_data"`
-	DeveloperData       *DeveloperDataItem  `json:"developer_data"`
-	PublicInterestStats *PublicInterestItem `json:"public_interest_stats"`
-	StatusUpdates       *[]StatusUpdateItem `json:"status_updates"`
-	LastUpdated         string              `json:"last_updated"`
-	Tickers             *[]TickerItem       `json:"tickers"`
+	BlockTimeInMin               int32               `json:"block_time_in_minutes"`
+	Categories                   []string            `json:"categories"`
+	Localization                 LocalizationItem    `json:"localization"`
+	Description                  DescriptionItem     `json:"description"`
+	Links                        *LinksItem          `json:"links"`
+	Image                        ImageItem           `json:"image"`
+	CountryOrigin                string              `json:"country_origin"`
+	GenesisDate                  string              `json:"genesis_date"`
+	SentimentVotesUpPercentage   float32             `json:"sentiment_votes_up_percentage"`
+	SentimentVotesDownPercentage float32             `json:"sentiment_votes_down_percentage"`
+	MarketCapRank                uint16              `json:"market_cap_rank"`
+	CoinGeckoRank                uint16              `json:"coingecko_rank"`
+	CoinGeckoScore               float32             `json:"coingecko_score"`
+	DeveloperScore               float32             `json:"developer_score"`
+	CommunityScore               float32             `json:"community_score"`
+	LiquidityScore               float32             `json:"liquidity_score"`
+	PublicInterestScore          float32             `json:"public_interest_score"`
+	MarketData                   *MarketDataItem     `json:"market_data"`
+	CommunityData                *CommunityDataItem  `json:"community_data"`
+	DeveloperData                *DeveloperDataItem  `json:"developer_data"`
+	PublicInterestStats          *PublicInterestItem `json:"public_interest_stats"`
+	StatusUpdates                *[]StatusUpdateItem `json:"status_updates"`
+	LastUpdated                  string              `json:"last_updated"`
+	Tickers                      *[]TickerItem       `json:"tickers"`
 }
 
 // CoinsIDTickers https://api.coingecko.com/api/v3/coins/steem/tickers?page=1
